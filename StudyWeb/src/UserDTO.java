0595a5ae4d3261965d6ca65b42d016131ffdbcba
YuhyeonMo <kora1492@naver.com>
2020-01-22 10:55:33 +0900

    UserDto and jsp modify

---
+++ StudyWeb/src/UserDTO.java
@@ -0,0 +1,25 @@
+
+public class UserDTO {
+	private String userID;
+	private String userName;
+	private String userEmail;
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	
+}
